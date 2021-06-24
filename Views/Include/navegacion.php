19debab89d43c1c4d1d9f851f5619903068fc990
Evelyn <evelynapuon@gmail.com>
2021-06-23 23:14:50 -0500

    Login_Terminado_sesiones

--- Views/Include/navegacion.php
+++ Views/Include/navegacion.php
@@ -1,3 +1,4 @@
+
                 <!-- Navbar -->
                 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
                     <!-- Left navbar links -->
@@ -12,38 +13,70 @@
                 <!-- Main Sidebar Container -->
                 <aside class="main-sidebar sidebar-dark-primary elevation-4">
                     <!-- Brand Logo -->
-                    <a href="../Views/principal.php" class="brand-link">
-                        <img src="dist/img/surtidora.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3">
+                    <?php if ($_SESSION['user'] == "Empleado") { ?>
+                    <a href="index3.html" class="brand-link">
+                        <img src="dist/img/letraE.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                         <span class="brand-text font-weight-light">Surtidora del Hogar</span>
                     </a>
-
+                   
                     <!-- Sidebar -->
                     <div class="sidebar">
                         <!-- Sidebar user panel (optional) -->
                         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                             <div class="image">
-                                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                                <img src="dist/img/empleados.png" class="img-circle elevation-2" alt="User Image">
                             </div>
+
+                            
+
                             <div class="info">
+                                <a href="../Views/principal.php" class="d-block">Empleado</a>
+                            </div>
+
+                            <?php }else if($_SESSION['user'] == "Administrador"){ ?>
+
+                                <a href="index3.html" class="brand-link">
+                        <img src="dist/img/letraA.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                        <span class="brand-text font-weight-light">Surtidora del Hogar</span>
+                    </a>
+                   
+                    <!-- Sidebar -->
+                    <div class="sidebar">
+                        <!-- Sidebar user panel (optional) -->
+                        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                            <div class="image">
+                                <img src="dist/img/admin.png" class="img-circle elevation-2" alt="User Image">
+                            </div>
+
+                                <div class="info">
                                 <a href="../Views/principal.php" class="d-block">Administrador</a>
                             </div>
+
+                             <?php } ?>
                         </div>
 
-                        <!-- Sidebar Menu -->
+                       <!----------------------------------Menú de Administrador --------------------------------->
+                          
+
+                                    <!-- Sidebar Menu -->
                         <nav class="mt-2">
                             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                                <!-- Add icons to the links using the .nav-icon class
+                              with font-awesome or any other icon font library -->
+                               
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-cart-arrow-down" style= "color: white;" ></i>
+                                        <i class="nav-icon fas fa-cart-arrow-down"></i>
                                         <p>
                                             Ventas
                                         </p>
                                     </a>
                                 </li>
+                                <?php if($_SESSION['user'] == "Administrador"){ ?>
 
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-box-open" style= "color: white;"></i>
+                                        <i class="nav-icon fas fa-box-open"></i>
                                         <p>
                                             Productos
                                         </p>
@@ -52,7 +85,7 @@
 
                                 <li class="nav-item">
                                     <a href="../Views/proveedores.php" class="nav-link">
-                                        <i class="nav-icon fa fa-truck" style= "color: white;"></i>
+                                        <i class="nav-icon fas fa-truck"></i>
                                         <p>
                                             Proveedores
                                         </p>
@@ -60,8 +93,8 @@
                                 </li>
 
                                 <li class="nav-item">
-                                    <a href="../Views/Clientes.php" class="nav-link">
-                                        <i class="nav-icon fas fa-users" style= "color: white;"></i>
+                                    <a href="#" class="nav-link">
+                                        <i class="nav-icon fas fa-user"></i>
                                         <p>
                                             Clientes
                                         </p>
@@ -70,26 +103,26 @@
 
                                 <li class="nav-item has-treeview">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-file-alt" style= "color: white;"></i>
+                                        <i class="nav-icon fas fa-file-alt"></i>
                                         <p>
                                             Reportes
-                                            <i class="fas fa-angle-left right" style= "color: white;"></i>
+                                            <i class="fas fa-angle-left right"></i>
                                         </p>
                                     </a>
                                     <ul class="nav nav-treeview">
                                         <li class="nav-item">
                                             <a href="pages/layout/top-nav.html" class="nav-link">
-                                                <i class="far fa-circle nav-icon" style= "color: white;"></i>
+                                                <i class="far fa-circle nav-icon"></i>
                                                 <p>Top Navigation</p>
                                             </a>
                                         </li>
                                         <li class="nav-item">
                                             <a href="pages/layout/boxed.html" class="nav-link">
-                                                <i class="far fa-circle nav-icon" style= "color: white;"></i>
+                                                <i class="far fa-circle nav-icon"></i>
                                                 <p>Boxed</p>
                                             </a>
                                         </li>
-                                      <!--  <li class="nav-item">
+                                        <li class="nav-item">
                                             <a href="pages/layout/fixed-sidebar.html" class="nav-link">
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <p>Fixed Sidebar</p>
@@ -112,22 +145,24 @@
                                                 <i class="far fa-circle nav-icon"></i>
                                                 <p>Collapsed Sidebar</p>
                                             </a>
-                                        </li>-->
+                                        </li>
                                     </ul>
                                 </li>
 
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-sliders-h" style= "color: white;"></i>
+                                        <i class="nav-icon fas fa-sliders-h"></i>
                                         <p>
                                             Ajustes
                                         </p>
                                     </a>
                                 </li>
-
+                                            
+                                <?php }?>
+                                <?php if ($_SESSION['user'] == "Empleado" || $_SESSION['user'] == "Administrador") { ?>
                                 <li class="nav-item">
-                                    <a href="../Views/Ayuda_Pdf/LaSurtidoraAyuda.pdf" class="nav-link">
-                                        <i class="nav-icon fas fa-info" style= "color: white;"></i>
+                                    <a href="#" class="nav-link">
+                                        <i class="nav-icon fas fa-info"></i>
                                         <p>
                                             Ayuda
                                         </p>
@@ -136,7 +171,7 @@
 
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-question" style= "color: white;"></i>
+                                        <i class="nav-icon fas fa-question"></i>
                                         <p>
                                             Acerca de
                                         </p>
@@ -145,12 +180,15 @@
 
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-sign-out-alt" style= "color: white;"></i>
+                                        <i class="nav-icon fas fa-sign-out-alt"></i>
                                         <p>
                                             Salir
                                         </p>
                                     </a>
                                 </li>
+                               
+                                <?php }?> 
+
 
                             </ul>
                         </nav>
@@ -158,3 +196,5 @@
                     </div>
                     <!-- /.sidebar -->
                 </aside>
+                
+               
--- Views/Principal.php
+++ Views/Principal.php
@@ -1,128 +1,43 @@
 <?php
-//require_once "../controllers/EstadisticasController.php";
-?>
+session_start();
+if (!isset($_SESSION['user'])) {
+  header('Location: login.php');
+}
 
+?>    
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <?php include "include/cabezera.php";?>
+    <?php include('Include/cabezera.php'); ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
 
-        <?php include "include/navegacion.php"?>
+        <!-- navegacion -->
+
+        <?php include('Include/navegacion.php'); ?>
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-12">
-                            <h1 class="m-0 text-dark">¡Bievenido al sistema!</h1>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
             <!-- Main content -->
             <section class="content">
-                <div class="container-fluid">
-
-                    <!-- Small boxes (Stat box) -->
-                    <div class="row">
-
-                        <!-- USUARIO -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-0 bg-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-dark text-uppercase mb-1">Proveedores</div>
-                                            <div class="h5 mb-0 font-weight-bold text-white"></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-truck fa-2x text-white"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- PRODUCTOS -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card bg-secondary border-0 shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-dark text-uppercase mb-1">Productos</div>
-                                            <div class="h5 mb-0 font-weight-bold text-white"></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-box-open fa-2x text-white"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- CLIENTES -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-0 bg-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-dark text-uppercase mb-1">Clientes</div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-white"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-users fa-2x text-white"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- VENTAS -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-0 bg-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-dark text-uppercase mb-1">Ventas</div>
-                                            <div class="h5 mb-0 font-weight-bold text-white"></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-cart-arrow-down fa-2x text-white"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                    </div>
-                    <!-- ./col -->
+                <div class="row" id="contenido_principal">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">PRINCIPAL</h1>
+                    </div><!-- /.col -->
                 </div>
+            </section>
+            <!-- /.content -->
+        </div>
 
-        </section>
-        </div><!-- /.container-fluid -->
-
-        <!-- /.content -->
+        <!-- footer -->
+        <?php include('Include/footer.php'); ?>
     </div>
-
-
-    <?php include "include/footer.php"?>
     <!-- ./wrapper -->
-    <?php include "Include/scripts.php";?>
+
+    <?php include('Include/scripts.php'); ?>
 </body>
+
 </html>
